da01edd540c22d772aa1fa4cae9d2f42ef37d2ff
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-12 11:11:34 +0500

    feat: created new components & one reusable Button component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ export default function App() {
       <div className="app">
         <div className="sidebar">
           <FriendsList />
+          <FormAddFriend />
+          <Button>Add Friend</Button>
         </div>
+
+        <FormSplitBill />
       </div>
     </>
   );
@@ -65,7 +69,57 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
+}
+
+// FormAddFriend component
+function FormAddFriend() {
+  return (
+    <>
+      <form className="form-add-friend">
+        <label>👫 Friend Name</label>
+        <input type="text" />
+
+        <label>🌄 Image URL</label>
+        <input type="text" />
+
+        <Button>Add</Button>
+      </form>
+    </>
+  );
+}
+
+// FormSplitBill
+function FormSplitBill() {
+  return (
+    <>
+      <form className="form-split-bill">
+        <h2>Split a bill with X</h2>
+
+        <label>💰 Bill value</label>
+        <input type="text" />
+
+        <label>🧑‍🦱 Your expense</label>
+        <input type="text" />
+
+        <label>👫 X's expense</label>
+        <input type="text" disabled />
+
+        <label>🤑 Who's paying the bill ?</label>
+        <select>
+          <option value="user">You</option>
+          <option value="friend">X</option>
+        </select>
+
+        <Button>Split bill</Button>
+      </form>
+    </>
+  );
+}
+
+// Button component
+function Button({ children }) {
+  return <button className="button">{children}</button>;
 }
